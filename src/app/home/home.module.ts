fe7bbdb8731dd68d2b97cf9f6419cc9031ddcab8
Gabriel Simon <gabrielsimon775@gmail.com>
2022-01-18 21:32:13 -0300

    Adicionadas validações na tela de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { SignInComponent } from './signin/signin.component';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     SignInComponent,
+  ],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
   ]
 })
 
